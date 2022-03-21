4f4f4b89ac64b31574b141539a1ad1040e2a9036
Anton <maksakov.anton@yandex.ru>
2022-03-21 21:59:13 +0300

    run

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int bonusScale = 20;
+
+        int bonusMile = cost / bonusScale;
+
+        return bonusMile;
+    }
+}
